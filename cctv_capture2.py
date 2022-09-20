cdb7c86486c2c4e5a3e410c62b36dcf63f0ca499
skyful-cpu <jhkang51@hotmail.com>
2022-09-20 16:02:39 +0900

    메인 실행 파일 v2

--- cctv_capture2.py
+++ cctv_capture2.py
@@ -1,4 +1,5 @@
 import argparse
+import copy
 from src.VideoFrame import VideoFrame
 
 OPENCV_MODE = "OPENCV"
@@ -23,27 +24,33 @@ CCTV_COMMAND = ['ffmpeg',
 
 CAMERA_SOURCE = 'rtsp://admin:hikvision123@192.168.11.145:554/ISAPI/streaming/channels/101'
 
-def main_function(capture_mode, width, height, count, time_interval):
+def main_function(capture_mode, camera_source, width, height):
     while True:
-        print(f"capture mode: {capture_mode} / width: {width} / height: {height} / autocapture count: {count} / time interval: {time_interval}")
-        print("IPCAM / WEBCAM screenshot program...")
-        print("PLEASE ENTER YOUR COMMAND...")
+        coppied_command = copy.deepcopy(CCTV_COMMAND)
+        coppied_command[2] = camera_source
+        print("\n***** IPCAM / WEBCAM screenshot program *****\n")
+        print("PLEASE ENTER YOUR COMMAND")
         cmd = input("1 to start, 0 to finish program... ")
+        
 
         if cmd == "1":
             video_frame = VideoFrame()
 
+            print("-" * 50 + "\n")
             video_frame.set_capture_mode(capture_mode=capture_mode)
             video_frame.set_ffmpeg_command(ffempeg_command=CCTV_COMMAND)
-            video_frame.set_camera_source(CAMERA_SOURCE)
+            video_frame.set_camera_source(camera_source)
             video_frame.set_frame_size(width=int(width), height=int(height))
+            print("\n" + "-" * 50)
+            count, time_interval = input("\nEnter auto screenshot count and time interval...").split()
+            print("")
             video_frame.set_autocapture(count=int(count), time_interval=int(time_interval))
 
             video_frame.connect_camera()
             video_frame.get_frame()
         
         elif cmd == "0":
-            print("exit program...")
+            print("***** exit program *****")
             break
 
         else:
@@ -52,11 +59,10 @@ def main_function(capture_mode, width, height, count, time_interval):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--mode", required=True, help="option: OPENCV / FFMPEG")
-    parser.add_argument("--width", required=True, help="frame width")
-    parser.add_argument("--height", required=True, help="frame height")
-    parser.add_argument("--count", required=True, help="how many auto screenshot you want to take")
-    parser.add_argument("--interval", required=True, help="time interval between each auto screenshot")
+    parser.add_argument("--source", help="webcam index, RTSP/HTTP address or other camera source", default='rtsp://admin:hikvision123@192.168.11.145:554/ISAPI/streaming/channels/101')
+    parser.add_argument("--width", help="frame width (needed when using in FFMPEG mode)", default=1080)
+    parser.add_argument("--height", help="frame height (needed when using in FFMPEG mode)", default=1920)
 
     args = parser.parse_args()
 
-    main_function(args.mode, args.width, args.height, args.count, args.interval)
+    main_function(args.mode, args.source, args.width, args.height)
